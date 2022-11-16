af0368c63f2c7f0c601442543dc33761eb7e6a08
ACER <ACER@DESKTOP-QH5VR0V>
2022-11-16 16:25:47 +0700

    add class node

--- Act5_DoubleLinkedList_149/Program.cs
+++ Act5_DoubleLinkedList_149/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿using System;
+
+namespace DoubleLinkedList
+{
+    class Node
+    {
+        /*Node class represents the node of double linked list
+         * It consists of the information part and links to
+         * its succeeding and preceeding nodes
+         * in terms of next and previous nodes.*/
+        public int rollNumber;
+        public string name;
+        public Node next;/*points to the succeeding node*/
+        public Node prev;/*points to the preceeding node*/
+    }
+}
