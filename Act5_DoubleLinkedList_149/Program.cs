49e19871e0e37df08038aeec33c83834c1d081ef
ACER <ACER@DESKTOP-QH5VR0V>
2022-11-16 16:39:58 +0700

    add class node

--- Act5_DoubleLinkedList_149/Program.cs
+++ Act5_DoubleLinkedList_149/Program.cs
@@ -13,4 +13,40 @@ namespace DoubleLinkedList
         public Node next;/*points to the succeeding node*/
         public Node prev;/*points to the preceeding node*/
     }
+    class DoubleLinkedList
+    {
+        Node START;
+        public DoubleLinkedList()
+        {
+            START = null;
+        }
+        public void addNode()/*Adds a new node*/
+        {
+            int rollNo;
+            string nm;
+            Console.Write("\nEnter the roll number of the student: ");
+            rollNo = Convert.ToInt32(Console.ReadLine());
+            nm = Console.ReadLine();
+            Node newnode = new Node();
+            newnode.rollNumber = rollNo;
+            newnode.name = nm;
+            /*Checks if the list is empty*/
+            if (START == null || rollNo <= START.rollNumber)
+            {
+                if ((START != null) && (rollNo == START.rollNumber))
+                {
+                    Console.WriteLine("\nDuplicate roll numbers not allowed");
+                    return;
+                    {
+                        newnode.next = START;
+                        if (START != null)
+                            START.prev = newnode;
+                        newnode.prev = null;
+                        START = newnode;
+                        return;
+                    }
+                }
+            }
+        }
+    }
 }
